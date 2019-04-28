2302ae1c4fef89ff5e8a843408346f11109a7bad
x00148542 <x00148542@outlook.com>
2019-04-28 21:26:41 +0100

    Changes to the profile and stock Management

--- target/scala-2.12/twirl/main/views/html/landlordProfile.template.scala
+++ target/scala-2.12/twirl/main/views/html/landlordProfile.template.scala
@@ -44,18 +44,16 @@ Seq[Any](_display_(/*2.2*/main("Profile", user)/*2.23*/ {_display_(Seq[Any](form
       </div>
       <div class="panel-body">
         <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4">
-                """),_display_(/*16.18*/if(env.resource("public/images/projectImages/" + landlord.getEmail + "thumb.jpg").isDefined)/*16.110*/ {_display_(Seq[Any](format.raw/*16.112*/("""
-                    """),format.raw/*17.21*/("""<img src="/assets/images/projectImages/"""),_display_(/*17.61*/(landlord.getEmail + "thumb.jpg")),format.raw/*17.94*/(""""/  id="profile-image1" class="img-circle img-responsive">
-                    """)))}/*18.23*/else/*18.28*/{_display_(Seq[Any](format.raw/*18.29*/("""
-                        """),format.raw/*19.25*/("""<img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive">
-                    """)))}),format.raw/*20.22*/("""
+              
+                        <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive">
+                    
           
 
 
-        """),format.raw/*24.9*/("""</div>
+        </div>
         <div class="col-md-8 col-xs-4">
           <div class="container">
-            <h2>"""),_display_(/*27.18*/landlord/*27.26*/.getFname()),format.raw/*27.37*/(""" """),_display_(/*27.39*/landlord/*27.47*/.getLname()),format.raw/*27.58*/("""</h2>
+            <h2>"""),_display_(/*25.18*/landlord/*25.26*/.getFname()),format.raw/*25.37*/(""" """),_display_(/*25.39*/landlord/*25.47*/.getLname()),format.raw/*25.58*/("""</h2>
             <p><b>Landlord</b></p>
 
 
@@ -64,7 +62,7 @@ Seq[Any](_display_(/*2.2*/main("Profile", user)/*2.23*/ {_display_(Seq[Any](form
           <ul class="container details">
             
             <li>
-              <p><span class="glyphicon glyphicon-envelope one" style="width:50px;"></span>"""),_display_(/*36.93*/landlord/*36.101*/.getEmail()),format.raw/*36.112*/("""</p>
+              <p><span class="glyphicon glyphicon-envelope one" style="width:50px;"></span>"""),_display_(/*34.93*/landlord/*34.101*/.getEmail()),format.raw/*34.112*/("""</p>
             </li>
           </ul>
           <hr>
@@ -78,39 +76,39 @@ Seq[Any](_display_(/*2.2*/main("Profile", user)/*2.23*/ {_display_(Seq[Any](form
 
 <h1>
   Houses
-  """),_display_(/*50.4*/if((user != null) && (("admin".equals(user.getRole())) || ("landlord".equals(user.getRole()))))/*50.99*/ {_display_(Seq[Any](format.raw/*50.101*/("""
-      """),format.raw/*51.7*/("""<a href=""""),_display_(/*51.17*/routes/*51.23*/.HomeController.addProperty()),format.raw/*51.52*/("""">
+  """),_display_(/*48.4*/if((user != null) && (("admin".equals(user.getRole())) || ("landlord".equals(user.getRole()))))/*48.99*/ {_display_(Seq[Any](format.raw/*48.101*/("""
+      """),format.raw/*49.7*/("""<a href=""""),_display_(/*49.17*/routes/*49.23*/.HomeController.addProperty()),format.raw/*49.52*/("""">
           <span style="font-size:20px">(Add New Property)</span>
       </a>
-  """)))}),format.raw/*54.4*/("""
-"""),format.raw/*55.1*/("""</h1>
+  """)))}),format.raw/*52.4*/("""
+"""),format.raw/*53.1*/("""</h1>
 <br>
 <div id="database-list">
   <table style="width:100%">
-      """),_display_(/*59.8*/for(property<-pList) yield /*59.28*/ {_display_(Seq[Any](format.raw/*59.30*/("""
-        """),_display_(/*60.10*/if(property.getLandlord() == landlord)/*60.48*/{_display_(Seq[Any](format.raw/*60.49*/("""
-          """),format.raw/*61.11*/("""<tr>
+      """),_display_(/*57.8*/for(property<-pList) yield /*57.28*/ {_display_(Seq[Any](format.raw/*57.30*/("""
+        """),_display_(/*58.10*/if(property.getLandlord() == landlord)/*58.48*/{_display_(Seq[Any](format.raw/*58.49*/("""
+          """),format.raw/*59.11*/("""<tr>
               <td>
-                  <a href=""""),_display_(/*63.29*/routes/*63.35*/.HomeController.viewProperty(property.getId)),format.raw/*63.79*/("""" class="listing">
-                  <img src=""""),_display_(/*64.30*/property/*64.38*/.getThumbnailPath),format.raw/*64.55*/("""" /> House for &euro;"""),_display_(/*64.77*/property/*64.85*/.getDisplayValue(property.getPrice)),format.raw/*64.120*/("""
-              """),format.raw/*65.15*/("""</td>
+                  <a href=""""),_display_(/*61.29*/routes/*61.35*/.HomeController.viewProperty(property.getId)),format.raw/*61.79*/("""" class="listing">
+                  <img src=""""),_display_(/*62.30*/property/*62.38*/.getThumbnailPath),format.raw/*62.55*/("""" /> House for &euro;"""),_display_(/*62.77*/property/*62.85*/.getDisplayValue(property.getPrice)),format.raw/*62.120*/("""
+              """),format.raw/*63.15*/("""</td>
                   <td class="desc">
-                      <i class="fas fa-bed"></i> """),_display_(/*67.51*/property/*67.59*/.getNumBeds),format.raw/*67.70*/("""&nbsp;
-                      <i class="fas fa-toilet"></i> """),_display_(/*68.54*/property/*68.62*/.getNumBaths),format.raw/*68.74*/("""&nbsp;
+                      <i class="fas fa-bed"></i> """),_display_(/*65.51*/property/*65.59*/.getNumBeds),format.raw/*65.70*/("""&nbsp;
+                      <i class="fas fa-toilet"></i> """),_display_(/*66.54*/property/*66.62*/.getNumBaths),format.raw/*66.74*/("""&nbsp;
 
-                      <i class="fas fa-leaf"></i>"""),_display_(/*70.51*/property/*70.59*/.getStock),format.raw/*70.68*/("""&nbsp;
+                      <i class="fas fa-leaf"></i>"""),_display_(/*68.51*/property/*68.59*/.getStock),format.raw/*68.68*/("""&nbsp;
                 
                           &nbsp;
-                          <a href=""""),_display_(/*73.37*/routes/*73.43*/.HomeController.updateProperty(property.getId)),format.raw/*73.89*/(""""><i class="fas fa-edit"></i></a>
+                          <a href=""""),_display_(/*71.37*/routes/*71.43*/.HomeController.updateProperty(property.getId)),format.raw/*71.89*/(""""><i class="fas fa-edit"></i></a>
                           &nbsp;
-                          <a href=""""),_display_(/*75.37*/routes/*75.43*/.HomeController.deleteProperty(property.getId)),format.raw/*75.89*/("""" onclick="return confirmDel();"><i class="fas fa-trash"></i></a>
+                          <a href=""""),_display_(/*73.37*/routes/*73.43*/.HomeController.deleteProperty(property.getId)),format.raw/*73.89*/("""" onclick="return confirmDel();"><i class="fas fa-trash"></i></a>
                       
                   </td>
               </a>
           </tr>
-      """)))}),format.raw/*80.8*/("""
-  """),format.raw/*81.3*/("""</table>
-""")))}),format.raw/*82.2*/("""
+      """)))}),format.raw/*78.8*/("""
+  """),format.raw/*79.3*/("""</table>
+""")))}),format.raw/*80.2*/("""
 """)))}))
       }
     }
@@ -127,11 +125,11 @@ Seq[Any](_display_(/*2.2*/main("Profile", user)/*2.23*/ {_display_(Seq[Any](form
 
               /*
                   -- GENERATED --
-                  DATE: Sun Apr 28 21:05:53 IST 2019
+                  DATE: Sun Apr 28 21:23:40 IST 2019
                   SOURCE: /home/wdd/temp/PlayReminder/app/views/landlordProfile.scala.html
-                  HASH: 11317a488d0dbcc32848b306949e5c886f651252
-                  MATRIX: 1043->1|1261->127|1290->148|1329->150|1357->152|1536->305|1584->345|1622->346|1663->359|1700->369|1715->375|1781->420|1855->463|1891->472|2060->614|2162->706|2203->708|2252->729|2319->769|2373->802|2472->883|2485->888|2524->889|2577->914|2793->1099|2842->1121|2967->1219|2984->1227|3016->1238|3045->1240|3062->1248|3094->1259|3359->1497|3377->1505|3410->1516|3653->1733|3757->1828|3798->1830|3832->1837|3869->1847|3884->1853|3934->1882|4046->1964|4074->1965|4172->2037|4208->2057|4248->2059|4285->2069|4332->2107|4371->2108|4410->2119|4489->2171|4504->2177|4569->2221|4644->2269|4661->2277|4699->2294|4748->2316|4765->2324|4822->2359|4865->2374|4984->2466|5001->2474|5033->2485|5120->2545|5137->2553|5170->2565|5255->2623|5272->2631|5302->2640|5422->2733|5437->2739|5504->2785|5634->2888|5649->2894|5716->2940|5901->3095|5931->3098|5971->3108
-                  LINES: 28->1|33->2|33->2|33->2|35->4|40->9|40->9|40->9|41->10|41->10|41->10|41->10|42->11|43->12|47->16|47->16|47->16|48->17|48->17|48->17|49->18|49->18|49->18|50->19|51->20|55->24|58->27|58->27|58->27|58->27|58->27|58->27|67->36|67->36|67->36|81->50|81->50|81->50|82->51|82->51|82->51|82->51|85->54|86->55|90->59|90->59|90->59|91->60|91->60|91->60|92->61|94->63|94->63|94->63|95->64|95->64|95->64|95->64|95->64|95->64|96->65|98->67|98->67|98->67|99->68|99->68|99->68|101->70|101->70|101->70|104->73|104->73|104->73|106->75|106->75|106->75|111->80|112->81|113->82
+                  HASH: a3e3d6f186efb06bf2d0444871fd11c3ac6e0d7d
+                  MATRIX: 1043->1|1261->127|1290->148|1329->150|1357->152|1536->305|1584->345|1622->346|1663->359|1700->369|1715->375|1781->420|1855->463|1891->472|2386->940|2403->948|2435->959|2464->961|2481->969|2513->980|2778->1218|2796->1226|2829->1237|3072->1454|3176->1549|3217->1551|3251->1558|3288->1568|3303->1574|3353->1603|3465->1685|3493->1686|3591->1758|3627->1778|3667->1780|3704->1790|3751->1828|3790->1829|3829->1840|3908->1892|3923->1898|3988->1942|4063->1990|4080->1998|4118->2015|4167->2037|4184->2045|4241->2080|4284->2095|4403->2187|4420->2195|4452->2206|4539->2266|4556->2274|4589->2286|4674->2344|4691->2352|4721->2361|4841->2454|4856->2460|4923->2506|5053->2609|5068->2615|5135->2661|5320->2816|5350->2819|5390->2829
+                  LINES: 28->1|33->2|33->2|33->2|35->4|40->9|40->9|40->9|41->10|41->10|41->10|41->10|42->11|43->12|56->25|56->25|56->25|56->25|56->25|56->25|65->34|65->34|65->34|79->48|79->48|79->48|80->49|80->49|80->49|80->49|83->52|84->53|88->57|88->57|88->57|89->58|89->58|89->58|90->59|92->61|92->61|92->61|93->62|93->62|93->62|93->62|93->62|93->62|94->63|96->65|96->65|96->65|97->66|97->66|97->66|99->68|99->68|99->68|102->71|102->71|102->71|104->73|104->73|104->73|109->78|110->79|111->80
                   -- GENERATED --
               */
           
